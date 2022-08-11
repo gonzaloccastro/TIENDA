27266e9aa1a3d953f1e00ae5081683685f561826
gonzaloccastro <gonzaloc.castro@gmail.com>
2022-08-10 21:46:11 -0300

    agrego estilo, en la proxima entrega empezare con la pagina web

--- script.js
+++ script.js
@@ -50,33 +50,36 @@ console.table(disponibles);
 const categoriaRopa=tienda.filter(articulo=>articulo.categoria=="ROPA");
 console.table(categoriaRopa);
 
-// FUNCION AGREGAR ARTICULO NUEVO
-
-function agregarArticulo(){
-let nombreArticuloNuevo=prompt("Ingrese el nuevo artículo:");
-let categoriaArticuloNuevo=prompt("Ingrese la categoría articulo:").toUpperCase();
-let autorArticuloNuevo=prompt("Ingrese de quien es el articulo:").toUpperCase();
-let precioArticuloNuevo=Number(prompt("Ingrese el precio del articulo:"));
-let disponibilidadArticuloNuevo=confirm("Ingrese si está disponible articulo:");
-let stockArticuloNuevo=Number(prompt("Ingrese el stock del articulo:"));
-let codigoArticuloNuevo=Number(prompt("Ingrese el código del articulo:"));
-let articuloNuevo=new articulo(nombreArticuloNuevo,categoriaArticuloNuevo,autorArticuloNuevo,precioArticuloNuevo, disponibilidadArticuloNuevo, stockArticuloNuevo, codigoArticuloNuevo);
-console.table(articuloNuevo);
-tienda.push(articuloNuevo);
-console.table(tienda);
-};
-
-let cantArticulosAgregar = parseInt(prompt("cuantos artículos queres agregar?"));
-for (let i=1;i<=cantArticulosAgregar;i++){
-    agregarArticulo();
-};
-
 // FIND PARA ENCONTRAR POR EL CODIGO UNICO
 
+let preguntaBuscar = confirm("Querés buscar un artículo por código?");
 let codigoBuscado=parseInt(prompt("Ingresa el código del artículo a buscar"));
 const porCodigo=tienda.find(art=>art.codigo===codigoBuscado);
 if(porCodigo==undefined){alert("Artículo no encontrado!");
     }else{console.table(porCodigo)};
+
+
+// FUNCION AGREGAR ARTICULO NUEVO
+
+function agregarArticulo(){
+    let nombreArticuloNuevo=prompt("Ingrese el nuevo artículo:");
+    let categoriaArticuloNuevo=prompt("Ingrese la categoría articulo:").toUpperCase();
+    let autorArticuloNuevo=prompt("Ingrese de quien es el articulo:").toUpperCase();
+    let precioArticuloNuevo=Number(prompt("Ingrese el precio del articulo:"));
+    let disponibilidadArticuloNuevo=confirm("Ingrese si está disponible articulo:");
+    let stockArticuloNuevo=Number(prompt("Ingrese el stock del articulo:"));
+    let codigoArticuloNuevo=Number(prompt("Ingrese el código del articulo:"));
+    let articuloNuevo=new articulo(nombreArticuloNuevo,categoriaArticuloNuevo,autorArticuloNuevo,precioArticuloNuevo, disponibilidadArticuloNuevo, stockArticuloNuevo, codigoArticuloNuevo);
+    console.table(articuloNuevo);
+    tienda.push(articuloNuevo);
+    console.table(tienda);
+    };
+    
+    let preguntaAgregar = confirm("Querés agregar un artículo?");
+    let cantArticulosAgregar = parseInt(prompt("Cuantos artículos queres agregar?"));
+    for (let i=1;i<=cantArticulosAgregar;i++){
+        agregarArticulo();
+    };
 
 // TABLA DE STOCK
 
