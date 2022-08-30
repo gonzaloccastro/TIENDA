ad091a577d5295e0e4cf13d7545eefe6f061c336
gonzaloccastro <gonzaloc.castro@gmail.com>
2022-08-30 00:57:13 -0300

    actualizo direcciones de fotos

--- productos.js
+++ productos.js
@@ -5,7 +5,7 @@ let productos = [{
         precio: 2500,
         disponibilidad: true,
         stock: 10,
-        imagen:"/media/tienda/remera.jpg",
+        imagen:"../media/tienda/remera.jpg",
         codigo: 1
     },
     {
@@ -15,7 +15,7 @@ let productos = [{
         precio: 2000,
         disponibilidad:  true,
         stock: 5,
-        imagen: '/media/tienda/gorra.jpg',
+        imagen: '../media/tienda/gorra.jpg',
         codigo: 2
     },
     {
@@ -25,7 +25,7 @@ let productos = [{
         precio: 1000,
         disponibilidad: true,
         stock: 3,
-        imagen: '/media/tienda/taza.jpg',
+        imagen: '../media/tienda/taza.jpg',
         codigo: 3
     },
     {
@@ -35,7 +35,7 @@ let productos = [{
         precio:  2000,
         disponibilidad:  true,
         stock: 8,
-        imagen:'/media/tienda/buzo.jpg',
+        imagen:'../media/tienda/buzo.jpg',
         codigo: 4
     },
     {
@@ -45,7 +45,7 @@ let productos = [{
         precio: 1000,
         disponibilidad: true,
         stock: 12,
-        imagen: '/media/tienda/cuaderno.jpg',
+        imagen: '../media/tienda/cuaderno.jpg',
         codigo: 5
     },
     {
@@ -55,7 +55,7 @@ let productos = [{
         precio: 3000,
         disponibilidad: true,
         stock: 12,
-        imagen: '/media/tienda/totebag.jpg',
+        imagen: '../media/tienda/totebag.jpg',
         codigo:5
     }
 ];
